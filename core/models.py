7e9b504e885dc8133fa78f640882f495fb79c882
@steve <asteve12335@gmail.com>
2023-12-21 17:13:28 +0100

    Creating user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.EmailField(unique=True)
