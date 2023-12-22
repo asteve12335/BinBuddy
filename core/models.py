64ca4fc62ee6669f524dce2046be13cb3ebe5891
@steve <asteve12335@gmail.com>
2023-12-22 16:18:55 +0100

    Creating basic user model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
+    username = models.CharField(max_length=55)
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
     email = models.EmailField(unique=True)
+    image = models.ImageField()
+    address = models.CharField(max_length=55)
+    contact = models.CharField(max_length=20,
+                               help_text='Enter phone number')
