9c9117e49f2e3b3f28db2887917317b1725ece9b
@steve <asteve12335@gmail.com>
2023-12-22 16:58:41 +0100

    Creation of order model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     username = models.CharField(max_length=55)
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
@@ -10,11 +19,28 @@ class User(models.Model):
     address = models.CharField(max_length=55)
     contact = models.CharField(max_length=20,
                                help_text='Enter phone number')
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1,
+                                  choices=MEMBERSHIP_CHOICES,
+                                  default=MEMBERSHIP_BRONZE)
 
     def __init__(self, *args, **kwargs):
         """Initializes a user"""
         super().__init__(*args, **kwargs)
 
 
-class Customer(User, models.Model):
-    pass
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1,
+                                      choices=PAYMENT_STATUS_CHOICES,
+                                      default=PAYMENT_STATUS_PENDING)
+
