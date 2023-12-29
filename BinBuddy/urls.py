8666a6d6a1acf94c01237e8dfa6c2c6636299024
@steve <asteve12335@gmail.com>
2023-12-29 01:35:16 +0100

    updating BinBuddy/urls module

--- BinBuddy/urls.py
+++ BinBuddy/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('core/', include('core.urls'))
+    # path('core/', include('core.urls'))
 ]
