04646612d92382b990dc018a2cda5352f04ed95c
@steve <asteve12335@gmail.com>
2024-01-09 23:10:02 +0100

    Core app created

--- core/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class ServiceProviderSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    username = serializers.CharField(max_length=255)
-    email = serializers.EmailField()
--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('serviceproviders/', views.service_provider_list),
-    path('serviceproviders/<int:id>/', views.service_provider_detail),
-]
